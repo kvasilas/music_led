dd2ff9132f9556af62f04f01aee1544a18880c41
KVasilas <kcvasilas@gmail.com>
2019-09-17 19:28:03 -0400

    uncomment

--- ind_add/tester/tester.ino
+++ ind_add/tester/tester.ino
@@ -11,25 +11,6 @@ uint8_t brightness = 255;
 
 #define UPDATES_PER_SECOND 100
 
-// This example shows several ways to set up and use 'palettes' of colors
-// with FastLED.
-//
-// These compact palettes provide an easy way to re-colorize your
-// animation on the fly, quickly, easily, and with low overhead.
-//
-// USING palettes is MUCH simpler in practice than in theory, so first just
-// run this sketch, and watch the pretty lights as you then read through
-// the code.  Although this sketch has eight (or more) different color schemes,
-// the entire sketch compiles down to about 6.5K on AVR.
-//
-// FastLED provides a few pre-configured color palettes, and makes it
-// extremely easy to make up your own color schemes with palettes.
-//
-// Some notes on the more abstract 'theory and practice' of
-// FastLED compact palettes are at the bottom of this file.
-
-
-
 CRGBPalette16 currentPalette;
 TBlendType    currentBlending;
 
@@ -113,6 +94,11 @@ void loop(){
         case 'k':
             Serial.println("Freedom - Linear");
             currentPalette = myRedWhiteBluePalette_p;
+            currentBlending = LINEARBLEND;
+            break;
+        case 'l':
+            Serial.println("yellow - Linear");
+            yelllow_setup();
             currentBlending = LINEARBLEND;
             break;
         case 'o':
@@ -227,3 +213,11 @@ const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
     CRGB::Black,
     CRGB::Black
 };
+
+void yelllow_setup()
+{
+    CRGB yellow = CHSV(HUE_Yellow, 150, 150);
+    CRGB black = CRGB::Black;
+
+    currentPalette = CRGBPalette16(yellow);
+}
